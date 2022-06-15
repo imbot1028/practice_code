b9c4c80feabe50d46c8b7b3128caebd6984f3da4
imbot1028 <imbot1028@gmail.com>
2022-06-16 00:24:30 +0900

    yt

--- 1.py
+++ 1.py
@@ -1,2 +1,9 @@
-import ccxt
-binance = ccxt.binance()
+from pytube import YouTube
+
+DOWNLOAD_FOLDER = "/Users/xinapse/Desktop/TTS"
+
+url = "hhttps://youtu.be/HHSUul3wJA4"
+
+yt = YouTube(url)
+stream = yt.streams.get_highest_resolution()
+stream.download(DOWNLOAD_FOLDER)
